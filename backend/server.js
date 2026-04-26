d0acf7f6ccde81244d1fcbb4dcdaf1d724aae58f
Gourav NSS <gourav341111@gmail.com>
2026-04-26 11:58:28 +0530

    codebase updated

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+const frontendDistPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(frontendDistPath));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendDistPath, "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
